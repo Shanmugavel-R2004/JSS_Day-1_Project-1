ad37f916ab1de53504026d855168c7212201409e
Shanmugavel R <shanmugavelramsamy@gmail.com>
2024-07-09 22:43:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('id1').addEventListener('click',function(){
+    document.getElementById("image").src="https://blogsofraghs.wordpress.com/files/2008/11/surya.jpg"});
+document.getElementById('id2').addEventListener('click',function(){
+    document.getElementById("image").src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNILnHS1E-oDpS2qqCykzQItlUEbiiwxFEFA&s"});
+document.getElementById('id3').addEventListener('click',function(){
+    document.getElementById("image").src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6CJqokKjyxcdmMdoIrTTRPHAfmh2yagYmCQ&s"});
+document.getElementById('id4').addEventListener('click',function(){
+    document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPcKygak2vwWGoBtK1UQZ8bEsSA79bzr2j5w&s" });
+document.getElementById('id5').addEventListener('click',function(){
+    document.getElementById("image").src="https://cdna.artstation.com/p/assets/images/images/034/717/384/large/amal-kakkat-1anjala1.jpg?1613030711"});
+document.getElementById('id6').addEventListener('click',function(){
+    document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdaNZ51I-GptkcBSjPX-ZSat-C-RakawoWYQ&s"});
+document.getElementById('id7').addEventListener('click',function(){
+    document.getElementById("image").src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJQSwLvU_ghj-9CC3Rrr8WDtf_qOTd57fxZg&s"});
+document.getElementById('id8').addEventListener('click',function(){
+    document.getElementById("image").src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrzytRoC_vLaVkZiceK-OlIDL0se-O0etBZg&s"});
